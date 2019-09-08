3c30f47c41f161025d7ab15bac03c024dc9d48a6
Ben Knight <benknight13@gmail.com>
2019-09-08 11:07:53 +0100

    Change version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.7"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Sends gcode based on twitch commands"""
+plugin_description = """Sends gcode based on twitch commands."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "ben knight"
